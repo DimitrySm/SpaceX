51178b6c67300b61ed6c6f7eec17c380189e47d8
Dmitriy Smaliak <you@example.com>
2021-01-19 02:53:09 +0300

    Updates

---
+++ precache-manifest.60c766a78d10be67bb5936fd08a9f510.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e948c211af6d8321c02c0cbff960910f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76073b5281d794da12d3",
+    "url": "/static/css/main.0ae9e3f7.chunk.css"
+  },
+  {
+    "revision": "8712d646cf58f682b08b",
+    "url": "/static/js/2.b7709456.chunk.js"
+  },
+  {
+    "revision": "88c0bfd11a2be5419414edbddb842b0f",
+    "url": "/static/js/2.b7709456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76073b5281d794da12d3",
+    "url": "/static/js/main.21cc4d2b.chunk.js"
+  },
+  {
+    "revision": "5268b1c7be79030bb5b5",
+    "url": "/static/js/runtime-main.142b1330.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/static/media/logo.d5ab31ce.svg"
+  }
+]);
